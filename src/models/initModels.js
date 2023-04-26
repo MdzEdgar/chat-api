0201468c106947303d19bb2c1d645f44f4001341
Edgar Méndez <mdzredgar@gmail.com>
2023-04-25 21:13:09 -0500

    Pendiente relaciones entre las tablas

---
+++ src/models/initModels.js
@@ -0,0 +1,21 @@
+//! TODO: Relaciones
+const Users = require('./users.models')
+const Conversations = require("./conversations.models")
+const Messages = require("./messages.models")
+const Participants = require("./participants.models")
+
+const initModels = () => {
+
+
+   //? Users -> Participants
+   Users.belongsTo(Participants)
+
+
+   //? Conversations -> Participants
+
+
+   //? Participants -> Messages
+
+}
+
+module.exports = initModels
